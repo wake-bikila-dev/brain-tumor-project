d20740af5f7db73cc1b0aaea34d6414b28614620
Wake Bikila <wake.bikila.dev@gmail.com>
2026-04-27 23:04:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def predict():
 
 if __name__ == '__main__':
     # Cloud environments inject the PORT environment variable
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
